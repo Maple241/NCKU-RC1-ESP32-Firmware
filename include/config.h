f9d8ce4bb95b7a20135ad94bdf5afc2f4a8621bd
Maple241 <114274814+Maple241@users.noreply.github.com>
2025-12-28 10:48:11 +0800

    Update config.h

--- include/config.h
+++ include/config.h
@@ -1,13 +1,21 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// 搖桿參數
 #define JOYSTICK_MAX 512
 #define JOYSTICK_MIN -512
 #define DEADZONE 30
-
-// PWM 參數
 #define PWM_MAX 255
-#define PWM_MIN 0
+
+// Pin defidition
+// 1. DRV8833
+// Left rear wheel
+#define PIN_MOTOR_L_IN1  26 
+#define PIN_MOTOR_L_IN2  27
+// Right rear wheel
+#define PIN_MOTOR_R_IN3  14
+#define PIN_MOTOR_R_IN4  12
+
+// 2. Servo
+#define PIN_SERVO        13
 
 #endif
